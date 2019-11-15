26c586f9469dedf0225fdc44cfb80500977da46f
Audun Meek Olsen <audunmeekolsen@gmail.com>
2019-11-15 22:59:58 +0100

    Add user deletion functionality

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,18 +1,63 @@
 <template>
-    <div class="Profile-wrapper">{{redan}}</div>
+    <div class="profile-wrapper">
+        <div>Hello, <span class="username">{{user.displayName}}</span>!</div>
+        <h1 class="delete-account" @click="deleteAccount">DELETE YOUR ACCOUNT</h1>
+    </div>
 </template>
 
 <script>
+import firebase from '@/firebaseinit';
 
 export default {
-    mounted() {
-        setTimeout(() => this.redan = "redan!!!!", 1000);
-    },
     data() {
         return {
-            redan: "not redan!!",
+            user: firebase.auth().currentUser
+        }
+    },
+    methods: {
+        async deleteAccount() {
+            
+            const deleteUser = firebase.functions().httpsCallable("deleteUser");
+            
+            const res = await Promise.resolve(deleteUser())
+                .catch(e => {
+                    console.error(e);
+                    alert("Could not delete user please try again.");
+                    return new Error();
+                });
+                
+            if (res instanceof Error) return;
+            
+            firebase.auth().signOut();
+            this.$router.push("/");
+            
+            
+            
+            // console.log(res.catch(err => console.log));
+            
+            // firebase.auth().currentUser.delete().then(function() {
+            //     firebase.analytics().logEvent("user_deleted");
+            // }).catch(function(error) {
+            //     alert('Could not delete account, only recently signed in accounts can be deleted. Please try to sign out, sign in and delete again.');
+            // });
+            
         }
     }
 }
 
 </script>
+
+<style scoped>
+
+    .username {
+        background: pink;
+        padding: 5px;
+    }
+    
+    .delete-account {
+        cursor: pointer;
+        text-decoration: underline;
+        color: blue;
+    }
+
+</style>
